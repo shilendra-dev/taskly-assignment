1706bf4f14ce25a5a1b14d85354b3e2d56afab3d
Shilendra Singh <business.shilendra@gmail.com>
2025-10-02 23:25:49 +0530

    updated axios instance

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 // Create axios instance
 export const api = axios.create({
-  baseURL: 'http://localhost:3001/api',
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3001/api',
   headers: {
     'Content-Type': 'application/json'
   }
