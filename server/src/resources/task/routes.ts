472f19da53531a6992cf6b86d79450b96c8398d4
Shilendra Singh <business.shilendra@gmail.com>
2025-10-02 10:41:09 +0530

    created updateTaskAPI

--- server/src/resources/task/routes.ts
+++ server/src/resources/task/routes.ts
@@ -1,6 +1,8 @@
 import { createApi } from "@/lib/ApiRouter";
 import { createTaskAPI } from "./api/create-task.api";
 import { getTasksAPI } from "./api/get-tasks.api";
+import { updateTaskAPI } from "./api/update-task.api";
 
 createApi().post('/task').authSecure(createTaskAPI);
 createApi().get('/tasks').authSecure(getTasksAPI);
+createApi().put('/task/:id').authSecure(updateTaskAPI);
